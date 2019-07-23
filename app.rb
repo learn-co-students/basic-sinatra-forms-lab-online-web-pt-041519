e98bef180177b94ec5fe21187d2c2689811522cd
github username <github email address>
2019-07-23 15:31:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
+  # Create a route that responds to a POST request at /team Have the form send a POST
+  # request to this route. Upon submission, pass the submitted data to the team.erb template.
 
 end
